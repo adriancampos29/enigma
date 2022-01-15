6e0d117d92d627462ad61ea81eddc1515bc2e868
Adrian Campos <adriancampos2992@yahoo.com>
2022-01-14 16:27:54 -0800

    Feature: add generate keys test with encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,4 +1,15 @@
-
 class Enigma
+
+  attr_reader :a_key
+  def initialize
+    @a_key = a_key
+    @character_set = ("a".."z").to_a << " "
+  end
+
+  def encrypt(message, key, date)
+    @a_key = key[0..1].to_i
+    
+  end
+
 
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -6,4 +6,20 @@ RSpec.describe Enigma do
     enigma = Enigma.new
     expect(enigma).to be_an_instance_of(Enigma)
   end
+
+  it 'can generate keys' do
+    enigma = Enigma.new
+    enigma.encrypt("hello world", "02715", "040895")
+    expect(enigma.a_key).to eq(02)
+    expect(enigma.b_key).to eq(27)
+    expect(enigma.c_key).to eq(71)
+    expect(enigma.d_key).to eq(15)
+  end
+
+
+  xit 'Encrypt a message with a key and date' do
+    enigma = Enigma.new
+      expect(enigma.encrypt("hello world", "02715", "040895")).to eq({encryption: "keder ohulw", key: "02715", date: "040895"})
+  end
+
 end
