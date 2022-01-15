09a4ea74b8fa7929f934bf958a5fa851e736c2e4
Adrian Campos <adriancampos2992@yahoo.com>
2022-01-14 16:31:28 -0800

    Feature: Generate keys test pass with method encrypt

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,14 +1,19 @@
 class Enigma
 
-  attr_reader :a_key
+  attr_reader :a_key, :b_key, :c_key, :d_key
   def initialize
     @a_key = a_key
+    @b_key = b_key
+    @c_key = c_key
+    @d_key = d_key
     @character_set = ("a".."z").to_a << " "
   end
 
   def encrypt(message, key, date)
     @a_key = key[0..1].to_i
-    
+    @b_key = key[1..2].to_i
+    @c_key = key[2..3].to_i
+    @d_key = key[3..4].to_i
   end
 
 
